4b5d540cf69243db8220dad2b92a8006e063dfbf
Dan Vanderkam <danvdk@gmail.com>
2018-04-04 09:47:44 -0400

    Remove dead code & deps (#9)
    * dead code removal
    
    * remove unused deps
    
    * add back @types/google.analytics

--- packages/transit/webpack.config.js
+++ packages/transit/webpack.config.js
@@ -1,5 +1,64 @@
-module.exports = require('../utils/base-webpack')(__dirname, {
+const path = require('path');
+const webpack = require('webpack');
+
+const isProd = process.env.NODE_ENV === 'production';
+
+const plugins = [
+  new webpack.DefinePlugin({
+    'process.env': {
+      NODE_ENV: JSON.stringify(process.env.NODE_ENV), // needs to be '"production"', quoted.
+    },
+  }),
+];
+if (isProd) {
+  plugins.push(new webpack.optimize.UglifyJsPlugin());
+}
+
+module.exports = {
   entry: {
     view: __dirname + '/src/view.tsx',
   },
-});
+  devtool: '#cheap-module-source-map',
+  output: {
+    path: __dirname + '/static',
+    filename: '[name].bundle.js',
+  },
+  module: {
+    preLoaders: [
+      {
+        test: /\.js$/,
+        loader: 'source-map-loader',
+      },
+    ],
+    loaders: [
+      {
+        test: /\.tsx?$/,
+        loader: 'awesome-typescript-loader',
+        options: {
+          transpileOnly: true,
+        },
+      },
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel',
+        query: {
+          presets: ['react', 'es2015'],
+        },
+      },
+      {
+        test: /\.json$/,
+        loader: 'json-loader',
+      },
+    ],
+  },
+  // Currently we need to add '.ts' to the resolve.extensions array.
+  resolve: {
+    extensions: ['', '.ts', '.tsx', '.webpack.js', '.web.js', '.js', '.jsx', '.json'],
+    root: path.resolve('./node_modules'),
+  },
+  resolveLoader: {
+    root: path.resolve('./node_modules'),
+  },
+  plugins: plugins,
+};
