d3b00739f0b02fe5b870b9730c4da3e2d1439a99
Dan Vanderkam <danvk@sidewalklabs.com>
2018-04-24 15:47:25 -0400

    cleanup

--- packages/transit/src/route-layer.tsx
+++ packages/transit/src/route-layer.tsx
@@ -3,7 +3,6 @@ import * as React from 'react';
 import {GeoJSONLayer} from 'react-mapbox-gl';
 
 import {shallowEqual, Feature, FeatureCollection} from '../../utils';
-import Colors from './colors';
 import {mixColors} from './utils';
 
 export interface Props {
@@ -18,6 +17,9 @@ interface State {
   transitFeatures: FeatureCollection;
 }
 
+// r5 returns route GeoJSON with separate two-point line features for each step.
+// Mapbox renders routes better if they're single LineString features, so we coalesce.
+// TODO: get r5 to return LineStrings directly?
 function coalesceFeatures(geojson: FeatureCollection): FeatureCollection {
   const features: Array<GeoJSONFeature<LineString>> = geojson.features as any;
 
@@ -37,66 +39,24 @@ function coalesceFeatures(geojson: FeatureCollection): FeatureCollection {
   return {type: 'FeatureCollection', features: outFeatures};
 }
 
-// Styles for steps and stops on a point-to-point route.
-function routeStyle(feature: Feature) {
-  const {properties} = feature;
-  const isWalk = !('tripId' in properties);
-  let pointRadius: number;
-  let pointColor: string;
-  let pointOutlineColor: string;
-  let pointOutlineWidth: number;
-  if (feature.geometry.type === 'Point') {
-    const {name} = properties;
-    if (name === 'Origin') {
-      pointColor = Colors.blackTransparent;
-      pointRadius = 8;
-    } else if (name === 'Destination') {
-      pointColor = Colors.blackTransparent;
-      pointRadius = 8;
-    } else {
-      pointColor = Colors.white; // station
-      pointOutlineColor = Colors.black;
-      pointOutlineWidth = 2;
-      pointRadius = 3;
-    }
-  }
-  return {
-    pointColor,
-    pointRadius,
-    pointOutlineColor,
-    pointOutlineWidth,
-    lineWidth: isWalk ? properties['stroke-width'] || 2 : properties['stroke-width'] || 4,
-    lineDash: isWalk ? [2, 4] : null, // Walks are dotted: 2px on, 4px off.
-    strokeOutlineColor: isWalk ? null : Colors.whiteTransparent,
-    strokeColor: properties['stroke'] || 'black',
-    strokeColorDark: mixColors(properties['stroke'] || 'black', '#000000'),
-  };
-}
-
 function makeStyledFeatures(geojson: FeatureCollection): FeatureCollection {
   return {
     type: 'FeatureCollection',
     features: geojson.features.map(f => {
-      const style = routeStyle(f);
+      const {properties} = f;
+      const stroke = properties.stroke || '#000000';
       return {
         ...f,
         properties: {
           ...f.properties,
-          lineColor: style.strokeColor || 'black',
-          lineColorDark: style.strokeColorDark || 'black',
-          lineWidth: style.lineWidth || 2,
+          lineColor: stroke,
+          lineOutlineColor: mixColors(stroke, '#000000'),
           isWalk: !('tripId' in f.properties),
         },
       };
     }),
   };
 }
-
-// Line style properties:
-// - line-color
-// - line-width
-// - line-gap-width: Draws a line casing outside of a line's actual path. Value indicates the width of the inner gap.
-// - line-dasharray: Specifies the lengths of the alternating dashes and gaps that form the dash pattern. The lengths are later scaled by the line width. To convert a dash length to pixels, multiply the length by the current line width.
 
 const LINE_PAINT_TRANSIT: mapboxgl.LinePaint = {
   'line-color': ['get', 'lineColor'],
@@ -105,7 +65,7 @@ const LINE_PAINT_TRANSIT: mapboxgl.LinePaint = {
 };
 
 const LINE_PAINT_TRANSIT_CASING: mapboxgl.LinePaint = {
-  'line-color': ['get', 'lineColorDark'],
+  'line-color': ['get', 'lineOutlineColor'],
   'line-width': 1,
   'line-gap-width': 4,
 };
@@ -116,27 +76,38 @@ const LINE_PAINT_WALK: mapboxgl.LinePaint = {
   'line-dasharray': [1, 1],
 };
 
-// Split into four layers:
-// - walks
-// - bicycle rides
-// - transit steps
-// - transit step outlines
+// TODO: bicycle route styling
 
-function filterFeatureCollection(
-  features: FeatureCollection,
+// Split a feature collection into two: one that satisfies the predicate and one that doesn't.
+function splitFeatureCollection(
+  featureCollection: FeatureCollection,
   predicate: (f: Feature) => boolean,
-): FeatureCollection {
-  return {
-    type: 'FeatureCollection',
-    features: features.features.filter(predicate),
-  };
+): [FeatureCollection, FeatureCollection] {
+  const results: Feature[][] = [[], []];
+  for (const feature of featureCollection.features) {
+    results[predicate(feature) ? 0 : 1].push(feature);
+  }
+  return [
+    {
+      type: 'FeatureCollection',
+      features: results[0],
+    },
+    {
+      type: 'FeatureCollection',
+      features: results[1],
+    },
+  ];
 }
 
 function propsToState(props: Props): State {
   const features = makeStyledFeatures(coalesceFeatures(props.geojson));
+  const [walkFeatures, transitFeatures] = splitFeatureCollection(
+    features,
+    f => f.properties.isWalk,
+  );
   return {
-    walkFeatures: filterFeatureCollection(features, f => f.properties.isWalk),
-    transitFeatures: filterFeatureCollection(features, f => !f.properties.isWalk),
+    walkFeatures,
+    transitFeatures,
   };
 }
 
