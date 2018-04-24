9986cabcced67f39041741eae26d7fea58616832
Dan Vanderkam <danvdk@gmail.com>
2018-04-24 11:22:09 -0400

    Fix issue where you couldn't drag the markers (#23)
    * proof of concept
    
    * works with all markers
    
    * handle mouse off
    
    * disable hover during drag
    
    * Disable fadeDuration
    
    * cleanup, comment
    
    * what samara said

--- packages/transit/src/choropleth-layer.tsx
+++ packages/transit/src/choropleth-layer.tsx
@@ -1,4 +1,3 @@
-import {LngLat} from 'mapbox-gl';
 import * as React from 'react';
 
 import {GeoJSONLayer} from 'react-mapbox-gl';
@@ -17,8 +16,6 @@ export interface Props {
   styleFn: StyleFn;
   visibility: 'visible' | 'none';
   before?: string;
-  onMouseHover?: (feature: Feature, lngLat: LngLat, map: mapboxgl.Map) => any;
-  onMouseLeave?: (map: mapboxgl.Map) => any;
 }
 
 interface State {
@@ -49,9 +46,6 @@ export class ChoroplethLayer extends React.Component<Props, State> {
     this.state = {
       styledFeatures: makeStyledFeatures(props.geojson, props.styleFn),
     };
-
-    this.onMouseMove = this.onMouseMove.bind(this);
-    this.onMouseLeave = this.onMouseLeave.bind(this);
   }
 
   render() {
@@ -60,29 +54,11 @@ export class ChoroplethLayer extends React.Component<Props, State> {
       <GeoJSONLayer
         id="choropleth"
         data={this.state.styledFeatures}
-        fillOnMouseEnter={this.onMouseMove}
-        fillOnMouseLeave={this.onMouseLeave}
-        fillOnMouseMove={this.onMouseMove}
         fillPaint={FILL_PAINT}
         fillLayout={{visibility}}
         before={before}
       />
     );
-  }
-
-  onMouseLeave(event: mapboxgl.MapMouseEvent) {
-    const {onMouseLeave} = this.props;
-    if (onMouseLeave) {
-      onMouseLeave(event.target);
-    }
-  }
-
-  onMouseMove(event: mapboxgl.MapMouseEvent) {
-    const {onMouseHover} = this.props;
-    if (onMouseHover) {
-      const feature = (event as any).features[0];
-      onMouseHover(feature, event.lngLat, event.target);
-    }
   }
 
   shouldComponentUpdate(nextProps: Props, nextState: State) {
