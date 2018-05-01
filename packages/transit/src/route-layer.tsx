667e69ee1ace52c9d8f1b8fe71987daa338f26ad
David Katz <david@sidewalklabs.com>
2018-05-01 11:49:50 -0400

    add comment deep copy

--- packages/transit/src/route-layer.tsx
+++ packages/transit/src/route-layer.tsx
@@ -21,6 +21,7 @@ interface State {
 // Mapbox renders routes better if they're single LineString features, so we coalesce.
 // TODO: get r5 to return LineStrings directly?
 function coalesceFeatures(geojson: FeatureCollection): FeatureCollection {
+  // Perform a deep copy to prevent appending to a modified geojson.
   const features: Array<GeoJSONFeature<LineString>> = JSON.parse(
     JSON.stringify(geojson.features),
   ) as any;
